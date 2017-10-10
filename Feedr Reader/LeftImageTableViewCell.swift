c64b6498f90bee3572324aba0034778fa707df2d
jfredrickson <jfredric@gmail.com>
2017-10-09 17:07:51 -0700

    p2: created view controllers in IB. Layout for feed wired up to IB Outlets.

---
+++ Feedr Reader/LeftImageTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  LeftImageTableViewCell.swift
+//  Feedr Reader
+//
+//  Created by Joshua Fredrickson on 10/9/17.
+//  Copyright © 2017 Joshua Fredrickson. All rights reserved.
+//
+
+import UIKit
+
+class LeftImageTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var storyImageView: UIImageView!
+    @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Feedr Reader/TopStoryTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TopStoryTableViewCell.swift
+//  Feedr Reader
+//
+//  Created by Joshua Fredrickson on 10/9/17.
+//  Copyright © 2017 Joshua Fredrickson. All rights reserved.
+//
+
+import UIKit
+
+class TopStoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var storyImage: UIImageView!
+    @IBOutlet weak var headlineLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
